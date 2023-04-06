fcc0b1c5cb4c272b0b8da4a70ccb418874223e12
Ben Thornton <smenjas@users.noreply.github.com>
2023-04-06 15:30:36 -0700

    Store tasks on the server instead of localStorage.

--- src/task.js
+++ src/task.js
@@ -3,6 +3,18 @@
 const fs = require('fs');
 
 module.exports = class Task {
+    static getTasks(name) {
+        try {
+            const json = fs.readFileSync(`../data/tasks/${name}.json`, 'utf8');
+            return JSON.parse(json);
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+        return {};
+    }
+
     static setTasks(name, tasks) {
         fs.writeFile(`../data/tasks/${name}.json`, JSON.stringify(tasks), error => {
             if (error) {
