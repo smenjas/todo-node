92f1ca7e47fcd6ce580233bc40a0dc7cd492aadc
Ben Thornton <smenjas@users.noreply.github.com>
2023-04-13 15:40:18 -0700

    createSalt(): Test salt entropy and randomness.
    Iterate 10,000 times checking for duplicates.
    
    If a 32 digit hexadecimal number contains:
    - only letters A-F, that's 83 bits of entropy
    - only numbers, that's 106 bits
    - both letters and numbers, that's 128 bits

--- tests/user.js
+++ tests/user.js
@@ -1,15 +1,23 @@
+import Common from '../src/common.js';
 import User from '../src/user.js';
 
 const tests = {};
 
 tests["Salt meets requirements."] = () => {
     let failures = [];
-    const salt = User.createSalt();
-    if (salt.length !== 32) {
-        failures.push(`Salt length is: ${salt.length}`);
-    }
-    if (!/^[0-9a-f]+$/.test(salt)) {
-        failures.push("Salt contains non-hexadecimal digits.");
+    let count = 0;
+    const salts = {};
+    while (count++ < 1e4) {
+        const salt = User.createSalt();
+        const entropy = Common.calculateEntropy(salt, true);
+        if (entropy < 128) { // 32 hexadecimal digits
+            failures.push(`Salt ${salt} entropy is: ${entropy}`);
+        }
+        if (salt in salts) {
+            failures.push(`Salt ${salt} appeared again!`);
+        } else {
+            salts[salt] = entropy;
+        }
     }
     return failures;
 };
