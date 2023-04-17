ad1814c8d91b456ce15ecf2d8c7a60e56767b316
Ben Thornton <smenjas@users.noreply.github.com>
2023-04-16 19:35:11 -0700

    Simplify login.js & edit-user.js.

--- public/js/edit-user.js
+++ public/js/edit-user.js
@@ -2,43 +2,40 @@ import AJAX from '/ajax.js';
 import Auth from '/auth.js';
 import Common from '/common.js';
 
-function editUser(data) {
-    AJAX.processForm('/edit-user', data, () => {
-        if (AJAX.request.readyState !== XMLHttpRequest.DONE) {
-            return;
-        }
-        if (AJAX.request.status !== 200 && AJAX.request.status !== 400) {
-            return;
-        }
-
-        const error = JSON.parse(AJAX.request.responseText);
-
-        const feedback = document.querySelector('#auth-feedback');
-        feedback.innerHTML = error ? error : 'You have changed your password.';
-    });
+function handleEditUserResponse() {
+    if (AJAX.request.readyState !== XMLHttpRequest.DONE) {
+        return;
+    }
+    if (AJAX.request.status !== 200 && AJAX.request.status !== 400) {
+        return;
+    }
+    const error = JSON.parse(AJAX.request.responseText);
+    const feedback = document.querySelector('#auth-feedback');
+    feedback.innerHTML = error ? error : 'You have changed your password.';
 }
 
-const form = document.querySelector('form#edit-user');
-if (form) {
-    const oldPasswordInput = document.querySelector('[name=password]');
-    const newPasswordInput = document.querySelector('[name=newPassword]');
+const form = document.querySelector('form');
+if (!form) {
+    throw new Error('form does not exist');
+}
 
-    const oldPasswordIndicator = document.querySelector('#valid-password');
-    const newPasswordIndicator = document.querySelector('#valid-newPassword');
+const oldPasswordInput = document.querySelector('[name=password]');
+const newPasswordInput = document.querySelector('[name=newPassword]');
+const oldPasswordIndicator = document.querySelector('#valid-password');
+const newPasswordIndicator = document.querySelector('#valid-newPassword');
 
-    Auth.validateInput(oldPasswordInput, oldPasswordIndicator, Common.validatePassword);
-    Auth.validateInput(newPasswordInput, newPasswordIndicator, Common.validatePassword);
-    Auth.checkPassword(newPasswordInput);
+Auth.validateInput(oldPasswordInput, oldPasswordIndicator, Common.validatePassword);
+Auth.validateInput(newPasswordInput, newPasswordIndicator, Common.validatePassword);
+Auth.checkPassword(newPasswordInput);
 
-    form.onsubmit = event => {
-        event.preventDefault();
-        const data = {
-            name: event.target.elements.name.value,
-            password: event.target.elements.password.value,
-            newPassword: event.target.elements.newPassword.value,
-        };
-        editUser(data);
-        event.target.elements.password.value = '';
-        event.target.elements.newPassword.value = '';
+form.onsubmit = event => {
+    event.preventDefault();
+    const data = {
+        name: event.target.elements.name.value,
+        password: event.target.elements.password.value,
+        newPassword: event.target.elements.newPassword.value,
     };
-}
+    AJAX.processForm('/edit-user', data, handleEditUserResponse);
+    event.target.elements.password.value = '';
+    event.target.elements.newPassword.value = '';
+};
--- public/js/login.js
+++ public/js/login.js
@@ -2,91 +2,48 @@ import AJAX from '/ajax.js';
 import Auth from '/auth.js';
 import Common from '/common.js';
 
-function createAccount(data) {
-    AJAX.processForm('/create-account', data, () => {
+function makeAuthHandler(statuses) {
+    return () => {
         if (AJAX.request.readyState !== XMLHttpRequest.DONE) {
             return;
         }
-        if (AJAX.request.status !== 201 && AJAX.request.status !== 409) {
+        if (!statuses.includes(AJAX.request.status)) {
             return;
         }
-
-        const error = JSON.parse(AJAX.request.responseText);
-
-        if (!error) {
-            window.location.href = '/';
+        error = JSON.parse(AJAX.request.responseText);
+        if (error) {
+            document.querySelector('#auth-feedback').innerHTML = error;
             return;
         }
+        window.location.href = '/';
+    };
+}
 
+const form = document.querySelector('form');
+if (!form) {
+    throw new Error('form does not exist');
+}
+
+let error;
+const nameInput = document.querySelector('[name=name]');
+const nameIndicator = document.querySelector('#valid-name');
+const passwordInput = document.querySelector('[name=password]');
+const passwordIndicator = document.querySelector('#valid-password');
+
+Auth.validateInput(nameInput, nameIndicator, Common.validateName);
+Auth.validateInput(passwordInput, passwordIndicator, Common.validatePassword);
+Auth.checkPassword(passwordInput);
+
+form.onsubmit = event => {
+    event.preventDefault();
+    const data = {
+        name: event.target.elements.name.value,
+        password: event.target.elements.password.value,
+    };
+    const url = `/${form.id}`;
+    const statuses = (form.id === 'login') ? [200, 401] : [201, 409];
+    AJAX.processForm(url, data, makeAuthHandler(statuses));
+    if (form.id === 'create-account') {
         Auth.signalValidity(nameInput, nameIndicator, !error);
-        feedback.innerHTML = error;
-    });
-}
-
-function logIn(data) {
-    AJAX.processForm('/login', data, () => {
-        if (AJAX.request.readyState !== XMLHttpRequest.DONE) {
-            return;
-        }
-        if (AJAX.request.status !== 200 && AJAX.request.status !== 401) {
-            return;
-        }
-
-        const error = JSON.parse(AJAX.request.responseText);
-
-        if (!error) {
-            window.location.href = '/';
-            return;
-        }
-
-        feedback.innerHTML = error;
-    });
-}
-
-const feedback = document.querySelector('#auth-feedback');
-
-let nameInput, passwordInput, nameIndicator, passwordIndicator;
-
-let form = document.querySelector('form#create-account');
-if (form) {
-    nameInput = document.querySelector('[name=name]');
-    passwordInput = document.querySelector('[name=password]');
-
-    nameIndicator = document.querySelector('#valid-name');
-    passwordIndicator = document.querySelector('#valid-password');
-
-    Auth.validateInput(nameInput, nameIndicator, Common.validateName);
-    Auth.validateInput(passwordInput, passwordIndicator, Common.validatePassword);
-    Auth.checkPassword(passwordInput);
-
-    form.onsubmit = event => {
-        event.preventDefault();
-        const data = {
-            name: event.target.elements.name.value,
-            password: event.target.elements.password.value,
-        };
-        createAccount(data);
-    };
-}
-
-form = document.querySelector('form#login');
-if (form) {
-    nameInput = document.querySelector('[name=name]');
-    passwordInput = document.querySelector('[name=password]');
-
-    nameIndicator = document.querySelector('#valid-name');
-    passwordIndicator = document.querySelector('#valid-password');
-
-    Auth.validateInput(nameInput, nameIndicator, Common.validateName);
-    Auth.validateInput(passwordInput, passwordIndicator, Common.validatePassword);
-    Auth.checkPassword(passwordInput);
-
-    form.onsubmit = event => {
-        event.preventDefault();
-        const data = {
-            name: event.target.elements.name.value,
-            password: event.target.elements.password.value,
-        };
-        logIn(data);
-    };
-}
+    }
+};
